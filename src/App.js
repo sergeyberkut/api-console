29c9c3ed65466960f6ba3defa6b003efe63b2b05
Sergey Berkut <serezha2145@gmail.com>
2020-06-02 21:32:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import { Route, Switch, Redirect, withRouter } from 'react-router-dom'
+import { AuthPage, ConsolePage } from './components/Pages'
+import Layout from './components/Layout'
+import Spinner from './components/Spinner'
+import { connect } from 'react-redux'
+import { autoLogin } from './store/actions/auth'
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ({ isAuthenticated, autoLogin, dataLoader }) => {
+
+	useEffect(() => {
+		autoLogin()
+	}, [])
+
+	if (dataLoader) return <Spinner />
+
+	let routes = (
+		<Switch>
+			<Route path='/' exact component={AuthPage} />
+			<Redirect to="/" />
+		</Switch>
+	)
+
+	if (isAuthenticated) {
+		routes = (
+			<Switch>
+				<Route path='/console' component={ConsolePage} exact />
+				<Redirect to="/console" />
+			</Switch>
+		)
+
+	}
+
+	return (
+		<Layout>
+			{routes}
+		</Layout>
+	)
 }
 
-export default App;
+const mapStateToProps = ({ auth: { isAuthenticated, dataLoader } }) => ({ isAuthenticated, dataLoader })
+
+const mapDispatchToProps = { autoLogin }
+
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { createStore, compose, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './store/reducers/rootReducer'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+)
+
+
+const app = (
+	<Provider store={store}>
+		<BrowserRouter>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</BrowserRouter>
+	</Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
+
